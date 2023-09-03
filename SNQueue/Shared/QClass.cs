8a665739cb303406575bb69228869aa13cb2a2cd
oasis <oasis@BS-HS-KIAB-0524>
2023-09-03 13:23:44 +0300

    Add project files.

---
+++ SNQueue/Shared/QClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SNQueue.Shared
+{
+    public class QClass
+    {
+        public static string? WorkingSiteName { get; set; } = "KING ABDULLAH HOSPITAL - REHAB";        
+        public class PatInfoGet
+        {
+            public Int32 userId { get; set; }=0;
+            public string? idType { get; set; } = string.Empty;
+            public string? idValue { get; set; } = string.Empty;
+        }
+    }
+}
